06de667a277caebf2471a478436f48636f9d2dbc
Eduardo Nagabe <eduardo_apha@hotmail.com>
2021-01-06 11:40:13 -0300

    GET /product

--- backend/src/main/java/com/devsuperior/dsdeliver/controllers/ProductController.java
+++ backend/src/main/java/com/devsuperior/dsdeliver/controllers/ProductController.java
@@ -14,15 +14,14 @@ import com.devsuperior.dsdeliver.services.ProductService;
 @RestController
 @RequestMapping(value = "/products")
 public class ProductController {
-	
+
 	@Autowired
 	private ProductService service;
-	
 	@GetMapping
 	public ResponseEntity<List<ProductDTO>> findAll(){
 		List<ProductDTO> list = service.findAll();
 		return ResponseEntity.ok().body(list);
+		
 	}
 	
-
 }
